ee039cb3f9c890c93f70513fc9f59fb1b6b3cd8c
Ilay Spector <ilaisp4@gmail.com>
2025-06-14 15:14:06 +0300

    add code to check if a file in a location is dangerous

--- main.py
+++ main.py
@@ -5,30 +5,38 @@ import requests
 API_KEY = os.environ.get("VIRUSTOTAL_API_KEY")
 
 
+def check_is_file_safe_with_virustotal(file_location: str, file_size_in_mb: int) -> bool:
+    url = "https://www.virustotal.com/api/v3/files"
+    if file_size_in_mb >= 32:
+        url = "https://www.virustotal.com/api/v3/files/upload_url"
+    
+    headers = {"x-apikey": API_KEY}
+    with open(file_location, "rb") as file:
+        files = {"file": file}
+        response = requests.post(url, headers=headers,files=files)
+    analysys_link = response.json()["data"]["links"]["self"]
+    analysys_response = requests.get(analysys_link, headers=headers)
+    analysys_data = analysys_response.json()["data"]["attributes"]["stats"]
+    return analysys_data["malicious"] != 0 or analysys_data["suspicious"] != 0
 
-def check_file_danger(location: str, api_key: str) -> bool:
-    pass
 
+def check_file_danger(file_location: str) -> bool:
+    if (not os.path.exists(file_location)):
+        raise Exception("Enter a proper file location")
+    
+    file_size_in_mb = os.path.getsize(file_location) // (1024 * 1024)
 
-def get_file_data(location: str) -> str:
-    pass
-
-
-def check_file_with_virustotal(file_data: str, api_key: str):
-    pass
-
-
-def validate_file_location(location: str) -> bool:
-    pass
+    
+    return check_is_file_safe_with_virustotal(file_location, file_size_in_mb)
 
 
 def main():
-    file_location = input("Enter file location: ")
-    print(f'The file "{file_location}" is ', end="")
-    if (check_file_danger(file_location, API_KEY)):
+    file_to_scan_location = input("Enter file location: ")
+    print(f'The file "{file_to_scan_location}" is ', end="")
+    if (check_file_danger(file_to_scan_location)):
         print("dangerous")
     else:
-        print("safe")
+        print("safe")   
 
 
 if __name__ == "__main__":
